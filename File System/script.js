c08f1007628bb790825dea99f2505cd082f2ca9d
Minhaz Uddin <uminhaz885@gmail.com>
2024-10-12 12:11:12 +0600

    append ,unlink(delete) files

--- File System/script.js
+++ File System/script.js
@@ -3,9 +3,10 @@
 2.create files/write files
 3.updtae files
 4.rename files
-5.
+5.append files
+6.rename files
   */}
-//write file
+//write file syntax --(filename.data,callback func(err))
 const fs = require('node:fs');
 fs.writeFile('myFile.txt', 'my name is Minhaz', (err) => {
   if (err) throw err;
@@ -15,4 +16,16 @@ fs.writeFile('myFile.txt', 'my name is Minhaz', (err) => {
 fs.writeFile('myIndex.html', 'my name is Minhaz', (err) => {
   if (err) throw err;
   console.log('File created');
+})
+//append file means 
+fs.appendFile('myFile.txt', 'i am an Eng student', (err) => {
+  if (err) throw err;
+  console.log('File appended');
+
+})
+
+//delete file
+fs.unlink('myIndex.html', (err) => {
+  if (err) throw err;
+  console.log('File deleted');
 })
