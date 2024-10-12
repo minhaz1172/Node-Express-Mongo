ad5e1c0003b2fb96cca47992909e944f0f13ec82
Minhaz Uddin <uminhaz885@gmail.com>
2024-10-13 00:04:39 +0600

    copy unlink read file

--- File System/script.js
+++ File System/script.js
@@ -1,10 +1,12 @@
 {/*common uses of file system module
 1.read files
 2.create files/write files
-3.updtae files
+3.update files/append
 4.rename files
 5.append files
 6.rename files
+7.copy file
+8.file open
   */}
 //write file syntax --(filename.data,callback func(err))
 const fs = require('node:fs');
@@ -28,4 +30,27 @@ fs.appendFile('myFile.txt', 'i am an Eng student', (err) => {
 fs.unlink('myIndex.html', (err) => {
   if (err) throw err;
   console.log('File deleted');
+})
+
+//read file redas data from the file
+fs.readFile('myFile.txt', 'utf8', (err, data) =>
+//utf8 for The encoding format. This ensures the file is read as a string, not raw binary data.
+{
+  if (err) throw err;
+
+  console.log("File saved", data);
+
+});
+
+//open file
+fs.open('myFile.txt', 'w', (err, file) => {
+  if (err) throw err;
+  console.log('file opened successfully', file);
+})
+
+//copy file--src,destination,callback
+fs.copyFile('myFile.txt', 'copy.txt', (err) => {
+
+  if (err) throw err;
+  console.log('file copied to copy.txt');
 })
