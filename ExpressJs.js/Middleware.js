d65887f2375e1a5f950718ff1a4d0a6fd4d4cfb6
Minhaz Uddin <uminhaz885@gmail.com>
2024-10-15 13:36:59 +0600

    error handle by middleware

--- ExpressJs.js/Middleware.js
+++ ExpressJs.js/Middleware.js
@@ -18,7 +18,19 @@ app.get('/profile', (req, res) => {
   res.send('My name is minhaz uddin ');
 })
 
+app.get('/about', (req, res, next) => {
+  res.send('profile showing');
+  return next(new Error("Somethig wrong"))//it will show in console
+})
+
+//error handling by middle ware
+//search middleare error handler expreess js in google
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')//sho in frontend
+})
 
 app.listen(3000, () => {
+  //optional callback func
   console.log("requst received")
 })
