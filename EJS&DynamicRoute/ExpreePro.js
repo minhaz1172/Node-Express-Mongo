1d5cc595de72b4612e6b89636a0ecd8990965919
Minhaz Uddin <uminhaz885@gmail.com>
2024-10-16 01:12:01 +0600

    Ejs html page shown by express ejs

---
+++ EJS&DynamicRoute/ExpreePro.js
@@ -0,0 +1,31 @@
+//dynamic routing
+//how to get data coming from frontend at backend route
+
+//setting up parsers for form handleing like login
+//setting up ejs for ejs pages //www.geeksforgeeks.com
+//setting up public static file(to use css,js,html in backend )
+
+//Ejs
+
+const express = require('express');
+const app = express();
+
+//settig parses for form handlng
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Set EJS as templating engine
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  // The render method takes the name of the html
+  // page to be rendered as input.
+  // This page should be in views folder 
+  // in the root directory.
+  
+    res.render('Index');//we have set index.ejs at view folder as we said view engine,ejs
+});
+
+ app.listen(4000, function () {
+  console.log('listening to port 4000')
+});
